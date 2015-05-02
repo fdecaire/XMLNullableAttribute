99764827b850d94af5df2ce71a92ede849dca89c
fdecaire <frank@decaire.net>
2015-05-02 11:12:23 -0400

    Initial check-in

---
+++ XMLNullableAttribute/House.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLNullableAttribute
+{
+	public class House
+	{
+		[XmlElement]
+		public List<Room> rooms = new List<Room>();
+	}
+
+	public class Room
+	{
+		[XmlAttribute(AttributeName = "name")]
+		public string Name { get; set; }
+
+		[XmlIgnore]
+		public int? NumberOfWindows { get; set; }
+
+		[XmlAttribute(AttributeName = "windows")]
+		public string WindowsSerializable
+		{
+			get
+			{
+				if (NumberOfWindows != null)
+				{
+					return NumberOfWindows.ToString();
+				}
+				else
+				{
+					return "";
+				}
+			}
+			set
+			{
+				if (WindowsSerializable != null)
+				{
+					NumberOfWindows = int.Parse(WindowsSerializable);
+				}
+			}
+		}
+		public bool ShouldSerializeWindowsSerializable()
+		{
+			return NumberOfWindows.HasValue;
+		}
+	}
+}
---
+++ XMLNullableAttribute/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace XMLNullableAttribute
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var document = new House();
+
+			// populate the data
+			document.rooms.Add(new Room { 
+				Name = "kitchen",
+				NumberOfWindows = 2
+			});
+
+			document.rooms.Add(new Room
+			{
+				Name = "bathroom",
+				NumberOfWindows = 0
+			});
+
+			document.rooms.Add(new Room
+			{
+				Name = "closet",
+				NumberOfWindows = null
+			});
+
+			// serialize the xml output
+			XmlSerializer ser = new XmlSerializer(typeof(House));
+			using (TextWriter writer = new StreamWriter(@"c:\testxml.xml"))
+			{
+				ser.Serialize(writer, document);
+			}
+		}
+	}
+}
